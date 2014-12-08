5d3e3361cb8a30680485ec2e89284a426b62770c
2ooom <dmitry.parfenchik@gmail.com>
2014-12-08 14:07:43 +0100

    adding grunt-watch

--- NotificationService.Client/Gruntfile.js
+++ NotificationService.Client/Gruntfile.js
@@ -1,7 +1,12 @@
-﻿/*
+﻿/// <binding AfterBuild='less' />
+/*
 This file in the main entry point for defining grunt tasks and using grunt plugins.
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
+
+var styles = {
+    files: { "wwwroot/css/styles.css": "app/assets/*.less" }
+}
 module.exports = function (grunt) {
     grunt.initConfig({
         bower: {
@@ -19,13 +24,23 @@ module.exports = function (grunt) {
                 options: {
                     paths: ["App/Assets"],
                 },
-                files: { "wwwroot/css/styles.css": "app/assets/*.less" } 
+                files: styles.files
             },
-        }
+        },
+        watch: {
+            styles: {
+                files: ['App/Assets/*.less'],
+                tasks: ['less'],
+                options: {
+                    spawn: false,
+                    interrupt: true,
+                },
+            },
+        },
     });
 
     grunt.registerTask("default", ["bower:install"]);
-
+    grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-bower-task");
     grunt.loadNpmTasks("grunt-contrib-less");
 };
