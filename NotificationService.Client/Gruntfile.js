345a3d72223a68a93826b052b5b5557e25ce7d15
2ooom <dmitry.parfenchik@gmail.com>
2014-12-08 13:14:52 +0100

    configuring bower

---
+++ NotificationService.Client/Gruntfile.js
@@ -0,0 +1,31 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: true
+                }
+            }
+        },
+        // Add this JSON object:
+        less: {
+            development: {
+                options: {
+                    paths: ["App/Assets"],
+                },
+                files: { "wwwroot/css/styles.css": "app/assets/*.less" } 
+            },
+        }
+    });
+
+    grunt.registerTask("default", ["bower:install"]);
+
+    grunt.loadNpmTasks("grunt-bower-task");
+    grunt.loadNpmTasks("grunt-contrib-less");
+};
